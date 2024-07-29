8e04ab4b51703be68a29dd431c6229db187e4d04
Alena <bloodykaryu@gmail.com>
2024-07-29 14:05:56 +0300

    Create Generation_password.py

---
+++ Generation_password.py
@@ -0,0 +1,28 @@
+def generate_password(length):
+    import random
+
+    ENG = [chr(i) for i in range(65, 91) if chr(i) != "O" and chr(i) != "I"]
+    eng = [chr(i) for i in range(97, 122) if chr(i) != "l" and chr(i) != "o"]
+    nums = [str(i) for i in range(2, 10)]
+    symbols = ENG + eng + nums
+    password = []
+    password.append(random.choice(ENG))
+    password.append(random.choice(eng))
+    password.append(random.choice(nums))
+    while len(password) < length:
+        password.append(random.choice(symbols))
+    random.shuffle(password)
+    return "".join(password)
+
+
+def generate_passwords(count, length):
+    passwords = []
+    for i in range(count):
+        password = generate_password(length)
+        passwords.append(password)
+    return passwords
+
+
+n, m = int(input()), int(input())
+
+print(*generate_passwords(n, m), sep="\n")
